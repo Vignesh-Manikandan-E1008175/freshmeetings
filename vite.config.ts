4a0cafcd9ced99fe4b48dd4d297cebe54f292279
Vignesh Manikandan <vignesh.manikandan@freshworks.com>
2023-11-05 21:36:09 +0530

    updated configs

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ export default defineConfig({
   build: {
     sourcemap: true,
     manifest: true,
+    cssCodeSplit: false,
     rollupOptions: {
       output: {
         entryFileNames: 'bundle.[hash].js'
