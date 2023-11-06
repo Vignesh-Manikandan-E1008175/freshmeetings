6d51d8b1d281d3807b813ed417f545a382c8c2bb
Vignesh Manikandan <vignesh.manikandan@freshworks.com>
2023-11-06 23:32:52 +0530

    react 18 -> 16

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
 import { StrictMode } from 'react'
-import ReactDOM from 'react-dom/client'
+import ReactDOM from 'react-dom'
 import App from './App.tsx'
 
 const rootEle = document.getElementById('root') as HTMLElement;
 
-ReactDOM.createRoot(rootEle).render(
+ReactDOM.render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+, rootEle)
